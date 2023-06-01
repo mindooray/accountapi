b4120020b4dc5f5615afb9b165ab2ef81184770f
lightbell03 <bellight28@gmail.com>
2023-06-01 17:42:54 +0900

    init

---
+++ src/main/java/com/nhnacademy/team4/accountapi/AccountapiApplication.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.team4.accountapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nhnacademy/team4/accountapi/AccountapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.team4.accountapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
