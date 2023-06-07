9b115d764ebb402de14082192904e130e1a36e99
김윤민 <ksw08130@naver.com>
2023-06-07 17:19:21 +0900

    email 400에러 수정

--- src/main/java/com/nhnacademy/team4/accountapi/exception/EmailNotExistException.java
+++ src/main/java/com/nhnacademy/team4/accountapi/exception/EmailNotExistException.java
@@ -3,7 +3,7 @@ package com.nhnacademy.team4.accountapi.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value = HttpStatus.NOT_FOUND)
+@ResponseStatus(value = HttpStatus.BAD_REQUEST)
 public class EmailNotExistException extends RuntimeException{
     public EmailNotExistException() { super("회원 이메일이 존재하지 않습니다.");}
 }
